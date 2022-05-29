36d0bb338579b26622cf111b02e2cfdd5a061411
Daniel Setiawan <95129220+danielsetiawan360@users.noreply.github.com>
2022-05-29 22:37:03 +0700

    Clean the code

--- java/script.js
+++ java/script.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-function chooseComputerSelection () {
-    number = (Math.floor(Math.random()*10)) 
-    if (number < 4) {
-        return 'rock' 
-=======
-const score = document.querySelector('#score')
+onst score = document.querySelector('#score')
 const score2 = document.querySelector('#score2')
 const news = document.querySelector('#news')
 
@@ -74,7 +68,6 @@ function chooseComputerSelection () {
     number = (Math.floor(Math.random()*10)) 
     if (number < 4) {
         return 'rock'
->>>>>>> rps-ui
     } else if (number < 7) {
         return 'paper'
     } else {
@@ -108,11 +101,7 @@ function decideRoundWinner() {
         return[status, message]
 }   else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         let status = 'computerWin'
-<<<<<<< HEAD
-        let message = 'You win!  Rocks beats scissors'
-=======
         let message = 'You lose!  Rock beats scissors'
->>>>>>> rps-ui
         return[status, message]
 }   else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         let status = 'playerWin'
@@ -122,74 +111,14 @@ function decideRoundWinner() {
         let status = 'tie'
         let message = 'It\'s a tie'
         return[status, message]
-<<<<<<< HEAD
-}   else
-        totalRound = totalRound-1;
-        console.log(totalRound)
-        let status = 'invalid'
-        let message = 'Please enter rock, paper, or scissors'
-        return[status, message]
-=======
 }
->>>>>>> rps-ui
 }
 
 function askForAnswers() {
     computerSelection = chooseComputerSelection()
-<<<<<<< HEAD
-    let playerSelectionCase = prompt('What is your choice?','')
-    if (playerSelectionCase == null) {
-        alert('Please enter something')
-        totalRound = totalRound-1;
-        console.log(totalRound)
-        return;
-    } else {
-        playerSelection = playerSelectionCase.toLowerCase()
-    }
-
-    let result = decideRoundWinner()
-    let status_beta = result[0]
-    let message_beta = result[1]
-    alert(message_beta)
-    return[status_beta]
-}
-
-function startGame() {
-    totalPlayerScore = 0
-    totalComputerScore = 0
-    let playerScore = 0
-    let computerScore = 0
-    totalRound = 0
-
-    for (;true;) {    
-        let whoWin = askForAnswers()
-        totalRound = totalRound + 1;
-        console.log (totalRound)    
-
-        if (whoWin == 'playerWin')  {
-            totalPlayerScore = ++playerScore
-        }   else if (whoWin == 'computerWin') {
-            totalComputerScore = ++computerScore
-        }   if (totalRound == 5) {
-            break;
-        }
-    }
-
-        if (totalPlayerScore > totalComputerScore) {
-            alert('You won! Congratulations!')
-        } else if (totalComputerScore > totalPlayerScore) {
-                alert('Oh no! Computer wins!')
-        } else {
-            alert('Whoops... It\'s a tie')
-        }
-     }
-
-startGame()
-=======
     let result = decideRoundWinner()
     let status_beta = result[0]
     let message_beta = result[1]
     news.textContent = message_beta
     return[status_beta]
 }
->>>>>>> rps-ui
